b97f55f857ed93d18b3d5c74b56442315228d03c
Michael Laszlo <git@michaellaszlo.com>
2016-11-19 21:04:02 -0500

    Parse text into canonical tokens

--- matcher.py
+++ matcher.py
@@ -1,5 +1,6 @@
 import json
 import os.path
+import string
 
 
 class Matcher:
@@ -8,14 +9,43 @@ class Matcher:
         print('%d products' % len(products))
         print('%d listings' % len(listings))
 
+letters = set(list(string.ascii_lowercase))
+digits = set(list(string.digits))
 
-class Product:
-    def __init__(self, data):
-        add_data(self, data, 'id', 'product_name', 'manufacturer', 'model')
+def make_token(char_set, text, pos):
+    chars = []
+    while True:
+        if pos == len(text) or text[pos] not in char_set:
+            break
+        chars.append(text[pos])
+        pos += 1
+    return pos, ''.join(chars)
 
-class Listing:
-    def __init__(self, data):
-        add_data(self, data, 'id', 'title', 'manufacturer')
+def make_canonical(text):
+    """Parse text into canonical tokens."""
+    tokens = []
+    text = text.lower()
+    pos = 0
+    while pos < len(text):
+        ch = text[pos]
+        made_token = False
+        for char_set in [ letters, digits ]:
+            if ch in char_set:
+                pos, token = make_token(char_set, text, pos)
+                tokens.append(token)
+                made_token = True
+                break
+        if not made_token:
+            pos += 1
+    print(text, tokens)
+
+def canonicalize_strings(item, *names):
+    """Convert the named attributes into lists of canonical tokens.""" 
+    item.canonical = Container()
+    for name in names:
+        text = getattr(item, name)
+        tokens = make_canonical(text) if text != None else None
+        setattr(item.canonical, name, tokens)
 
 def add_data(item, data, *names):
     """Set item attributes by getting named values from a data dictionary."""
@@ -23,6 +53,19 @@ def add_data(item, data, *names):
         value = data[name] if name in data else None
         setattr(item, name, value)
     item.data = data
+
+class Container:
+    pass
+
+class Product:
+    def __init__(self, data):
+        add_data(self, data, 'id', 'product_name', 'manufacturer', 'model')
+        canonicalize_strings(self, 'manufacturer', 'model')
+
+class Listing:
+    def __init__(self, data):
+        add_data(self, data, 'id', 'title', 'manufacturer')
+        canonicalize_strings(self, 'manufacturer', 'title')
 
 def load_items(Item, file_path):
     """Make a list of Item objects loaded from a file of JSON lines."""
@@ -40,7 +83,7 @@ def load_items(Item, file_path):
 if __name__ == '__main__':
     data_dir = 'data/dev'
     products_name = 'products.txt'
-    listings_name = 'listings.txt'
+    listings_name = 'listings_a.txt'
     products = load_items(Product, os.path.join(data_dir, products_name))
     listings = load_items(Listing, os.path.join(data_dir, listings_name))
     matcher = Matcher(products, listings)
