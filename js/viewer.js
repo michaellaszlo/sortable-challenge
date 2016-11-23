cf72676cbff5cfacd8eee454d793e6abb7dc60af
Michael Laszlo <git@michaellaszlo.com>
2016-11-23 03:54:19 -0500

    Debug case-sensitive highlighting

--- js/viewer.js
+++ js/viewer.js
@@ -15,7 +15,7 @@ var Viewer = (function () {
   function format(x, decimalDigits) {
     var s = '' + Math.round(Math.pow(10, decimalDigits) * x),
         pos = s.length - decimalDigits;
-    return s.substring(0, pos) + '.' + s.substring(pos);
+    return (pos == 0 ? '0' : s.substring(0, pos)) + '.' + s.substring(pos);
   }
 
   function load() {
@@ -63,7 +63,7 @@ var Viewer = (function () {
                 parent: groupBox }),
             listingBox = M.make('div', { className: 'listing',
                 parent: container }),
-            text, spans, i, a, b, token, tokenMap;
+            spans, i, a, b, text, textLower, token, tokenLower, tokenMap;
         // Display the listing.
         [ 'id', 'manufacturer', 'title' ].forEach(function (field) {
           var pair = M.make('div', { className: 'pair ' + field,
@@ -104,6 +104,7 @@ var Viewer = (function () {
             }
             // Inject highlighting into the product text.
             text = product[field].text;
+            textLower = text.toLowerCase();
             spans = product[field].tokenSpans;
             tokenMap = listing[field == 'manufacturer' ?
                 'manufacturer' : 'title'].tokenMap;
@@ -111,7 +112,7 @@ var Viewer = (function () {
               a = spans[i][0];
               b = spans[i][1];
               // Save the token to be highlighted later in the listing field.
-              token = text.substring(a, b);
+              token = textLower.substring(a, b);
               tokenMap[token] = field;
               text = text.substring(0, a) + '<span class="match ' + field +
                   '">' + text.substring(a, b) + '</span>' + text.substring(b);
@@ -126,12 +127,13 @@ var Viewer = (function () {
         [ 'manufacturer', 'title' ].forEach(function (field) {
           var target = listing[field];
           text = target.text;
+          textLower = text.toLowerCase();
           spans = target.tokenSpans;
           tokenMap = target.tokenMap;
           for (i = spans.length - 1; i >= 0; --i) {
             a = spans[i][0];
             b = spans[i][1];
-            token = text.substring(a, b);
+            token = textLower.substring(a, b);
             if (token in tokenMap) {
               text = text.substring(0, a) + '<span class="match ' + 
                   tokenMap[token] + '">' + text.substring(a, b) +
@@ -149,7 +151,7 @@ var Viewer = (function () {
     groupOrder.forEach(function (name) {
       var info = groupInfo[name],
           link = M.make('li', { parent: linkContainer,
-              innerHTML: info.headerText.replace(/\s/g, '&nbsp;') }),
+              innerHTML: info.headerText }),
           groupElement = info.element;
       link.onclick = function () {
         groupElement.scrollIntoView();
